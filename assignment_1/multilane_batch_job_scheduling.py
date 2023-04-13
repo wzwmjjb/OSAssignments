e5bf9cede4762f63db6776e21bcd12a3fbfbe2c7
wxr <2861646904@qq.com>
2023-04-13 13:38:08 +0800

    Task 1

--- assignment_1/multilane_batch_job_scheduling.py
+++ assignment_1/multilane_batch_job_scheduling.py
@@ -3,10 +3,10 @@ def preprocess_data(k, data):
     data = data.split('\n')
     for i in range(k):
         line = data[i].strip().split()
-        data_dict[line[0]] = line[1]
+        data_dict[i] = line
     return data_dict
+
 
 def MBJS(k, data):
     dd = preprocess_data(k, data)
     return dd
-
