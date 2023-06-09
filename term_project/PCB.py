39e4805c488d2dc3350657fe25827b4f6ce5f5d9
wxr <2861646904@qq.com>
2023-06-10 03:00:48 +0800

    [master] update

---
+++ term_project/PCB.py
@@ -0,0 +1,18 @@
+class PCB:
+    """
+    进程控制块
+    """
+    def __init__(self, pcb_id, status, count, x):
+        """
+        :param pcb_id:进程标识数
+        :param status:进程状态
+        :param count:要输出的文件数
+        :param x:进程输出时的临时变量
+        """
+        self.pcb_id = pcb_id
+        self.status = status
+        self.count = count
+        self.x = x
+
+    def __str__(self):
+        return "PCB(pcb_id=%d, status=%d, count=%d, x=%d)" % (self.pcb_id, self.status, self.count, self.x)
---
+++ term_project/request_block.py
@@ -0,0 +1,17 @@
+class ReqBlock:
+    """
+    请求输出块
+    """
+
+    def __init__(self, req_name, length, address):
+        """
+        :param req_name:请求进程名
+        :param length:本次输出信息长度
+        :param address:信息在输出井的首地址
+        """
+        self.req_name = req_name
+        self.length = length
+        self.address = address
+
+    def __str__(self):
+        return "ReqBlock(req_name=%s, length=%d, address=%d)" % (self.req_name, self.length, self.address)
