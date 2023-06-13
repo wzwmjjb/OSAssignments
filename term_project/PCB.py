e7ba10e130a498c15a6e844632bbbf8a3df722ff
wxr <2861646904@qq.com>
2023-06-13 22:17:52 +0800

    [master] update

--- term_project/PCB.py
+++ term_project/PCB.py
@@ -1,12 +1,14 @@
 import random
 
+from request_block import ReqBlock
+
 
 def random_x():
     """
     随机生成x，其中x的长度是随机生成的，且x中的字符也是1-9之间的随机数字，x以0结尾
     :return:
     """
-    length = random.randint(1, 30)
+    length = random.randint(1, 80)
     x = ""
     for i in range(length):
         x += str(random.randint(1, 9))
@@ -17,21 +19,26 @@ def random_x():
 class PCB:
     """
     进程控制块
+    :param pcb_id:进程标识数
+    :param status:进程状态
+    :param count:要输出的文件数
+    :param x:进程输出时的临时变量
+    :param doc:要输出的文件集合
+    :param doc_len:要输出的文件集合中每个文件的长度
+    :param current_doc:当前正在输出的文件指针
+    :param doc_first_address:要输出的文件集合中每个文件在输出井中的首地址
     """
 
     def __init__(self, pcb_id: int, status: int, count: int, x: str):
-        """
-        :param pcb_id:进程标识数
-        :param status:进程状态
-        :param count:要输出的文件数
-        :param x:进程输出时的临时变量
-        """
         self.pcb_id = pcb_id
         self.status = status
         self.count = count
         self.x = x
+
         self.doc = None
         self.doc_len = None
+        self.current_doc = 0
+        self.doc_first_address = []
 
     def __str__(self):
         return "PCB(pcb_id=%d, status=%d, count=%d, x=%s)" % (self.pcb_id, self.status, self.count, self.x)
@@ -44,27 +51,49 @@ class PCB:
             self.doc_len.append(len(self.doc[i]))
         self.x = self.doc[0]
 
-    def dispatch_user_process(self, remain_buffer, req_block_num, spooling_buffer, first_empty):
+    def dispatch_user_process(self, remain_buffer: int, req_block_num: int, spooling_buffer: list, first_empty: int,
+                              ptr1: int, req_block: ReqBlock):
         """
-        调度用户进程
+        调度用户进程，最多完成一个文件的输出，也有可能完成之前没完成的半个文件，或者一个新文件只完成了一半输出井就满了
         :param remain_buffer:输出井剩余容量 c1[i]
         :param req_block_num:空闲输出请求块数 c3
         :param spooling_buffer:输出井 spooling_pool[i]
         :param first_empty:第一个可用空缓冲指针 c2[i][0]
-        :return:
+        :param ptr1:空闲请求输出块指针 ptr1
+        :param req_block:输出请求块req_blocks[ptr1]
+        :return:c1[i], c3, c2[i][0], ptr1
         """
         while True:
             if remain_buffer == 0:  # 输出井满
                 self.status = 1
-                return
-            else:
+                return remain_buffer, req_block_num, first_empty, ptr1
+            else:  # 把文件里的字符输出到输出井
                 self.status = 0
-                spooling_buffer[first_empty] = self.x
-                first_empty = (first_empty + 1) / 100
+                spooling_buffer[first_empty] = self.x[0]
+                if len(self.doc_first_address) == self.current_doc:
+                    self.doc_first_address.append(first_empty)
+                first_empty = (first_empty + 1) % 100
                 remain_buffer -= 1
                 self.x = self.x[1:]
-                if self.x != "0":
+                if len(self.x) == 0:
                     break
+
+        self.current_doc += 1
+        if self.current_doc == self.count:  # 所有文件都输出完毕
+            self.status = 4
+            return remain_buffer, req_block_num, first_empty, ptr1
+        self.x = self.doc[self.current_doc]  # 更新x，读取下一个文件
+
         if req_block_num == 0:  # 没有空闲输出请求块
             self.status = 3
-            return
+            return remain_buffer, req_block_num, first_empty, ptr1
+
+        req_block.req_name = self.pcb_id
+        # TODO 文件长度大于输出井长度？？？？？先默认不会好了
+        req_block.req_length = self.doc_len[self.current_doc - 1]
+        req_block.req_address = first_empty
+        req_block.req_address = self.doc_first_address[self.current_doc - 1]
+        req_block_num -= 1
+        ptr1 += 1
+        return remain_buffer, req_block_num, first_empty, ptr1
+
--- term_project/spooling_server.py
+++ term_project/spooling_server.py
@@ -1,20 +1,21 @@
 import random
 
+from request_block import ReqBlock
 from term_project.PCB import PCB
 
 
 class SpoolingServer:
     """
     SPOOLing输出服务程序
-    : param user0:用户进程0
-    : param user1:用户进程1
-    : param spooling_pool:输出井
-    : param c1:输出井可用容量
-    : param c2:输出井使用情况 [第一个可用空缓冲指针, 第一个满缓冲指针]
-    : param c3:输出请求块数
-    : param ptr0:要输出的第一个请求输出块指针
-    : param ptr1:空闲请求输出块指针
-    : param spooling_process:SPOOLing执行进程
+    :param user0:用户进程0
+    :param user1:用户进程1
+    :param spooling_pool:输出井
+    :param c1:输出井可用容量
+    :param c2:输出井使用情况 [第一个可用空缓冲指针, 第一个满缓冲指针]
+    :param c3:输出请求块数
+    :param ptr0:要输出的第一个请求输出块指针
+    :param ptr1:空闲请求输出块指针
+    :param spooling_process:SPOOLing执行进程
     """
 
     def __init__(self, file_num0: int, file_num1: int):
@@ -24,15 +25,19 @@ class SpoolingServer:
         self.user0.documents()
         self.user1.documents()
         # 初始化两个用户的输出井
-        self.spooling_pool = [[], []]
+        self.spooling_pool = [[" " for i in range(100)], [" " for i in range(100)]]
         self.c1 = [100, 100]
         self.c2 = [[0, 0], [0, 0]]
         # 初始化输出请求块
         self.c3 = 10
+        self.req_blocks = [ReqBlock(0, 0, 0) for i in range(self.c3)]
         self.ptr0 = 0
         self.ptr1 = 0
         # 初始化SPOOLing执行进程
-        self.spooling_process = PCB(2, 0, 0, "")
+        self.spooling_process = PCB(2, 2, 0, "")
+
+        # 调度信息
+        self.dispatch_info = []
 
     def random_dispatch(self):
         """
@@ -41,23 +46,52 @@ class SpoolingServer:
         x = random.randint(1, 100)
         if x <= 45:
             if self.user0.status == 0:  # 执行用户进程0
-                pass
+                self.c1[0], self.c3, self.c2[0][0], self.ptr1 = self.user0.dispatch_user_process(self.c1[0], self.c3,
+                                                                                                 self.spooling_pool[0],
+                                                                                                 self.c2[0][0],
+                                                                                                 self.ptr1,
+                                                                                                 self.req_blocks[
+                                                                                                     self.ptr1])
+                if self.spooling_process.status != 0:
+                    self.spooling_process.status = 0
+                self.dispatch_info.append("执行用户进程0，输出字符到输出井，处于执行状态0\n")
             elif self.user0.status == 1:  # 输出井0满
-                pass
+                self.dispatch_info.append("用户进程0输出井0满，处于等待状态1\n")
             elif self.user0.status == 3:  # 没有空闲输出请求块
-                pass
+                self.dispatch_info.append("用户进程0没有空闲输出请求块，处于等待状态3\n")
             else:
                 pass
         elif x <= 90:
-            pass
-        elif self.spooling_process.status == 0:
-            pass  # 执行SPOOLing进程
+            if self.user1.status == 0:
+                self.c1[1], self.c3, self.c2[1][0], self.ptr1 = self.user1.dispatch_user_process(self.c1[1], self.c3,
+                                                                                                 self.spooling_pool[1],
+                                                                                                 self.c2[1][0],
+                                                                                                 self.ptr1,
+                                                                                                 self.req_blocks[
+                                                                                                     self.ptr1])
+                if self.spooling_process.status != 0:
+                    self.spooling_process.status = 0
+                self.dispatch_info.append("执行用户进程1，输出字符到输出井，处于执行状态0\n")
+            elif self.user1.status == 1:
+                self.dispatch_info.append("用户进程1输出井1满，处于等待状态1\n")
+            elif self.user1.status == 3:
+                self.dispatch_info.append("用户进程1没有空闲输出请求块，处于等待状态3\n")
+            else:
+                pass
+        elif x <= 100:
+            if self.spooling_process.status == 0:
+                # TODO 等会写
+                self.dispatch_info.append("SPOOLing执行进程处于执行状态0\n")
+            elif self.spooling_process.status == 2:
+                self.dispatch_info.append("输出井空，SPOOLing执行进程处于等待状态2\n")
+            elif self.spooling_process.status == 4:
+                self.dispatch_info.append("SPOOLing执行进程已经完成，处于终止状态4\n")
         else:
             pass
 
 
 if __name__ == '__main__':
     spooling = SpoolingServer(3, 5)
-    while (spooling.user0.status != 4 or spooling.user1.status != 4):
+    while spooling.spooling_process.status != 4:
         spooling.random_dispatch()
     print("hh")
